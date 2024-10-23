d966fe192b06fcde9feed0e8bbeb07689fba6315
Abu Yousuf Siam <abuyousufsiam7249@gmail.com>
2024-10-23 15:18:45 +0600

    navbar & header added with motion(to be updated later for more customization)

---
+++ main.js
@@ -0,0 +1,29 @@
+// Toggle Button
+const navMenu = document.getElementById("nav-menu")
+const navLink = document.querySelectorAll(".nav-link")
+const hamburger = document.getElementById("hamburger") 
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]")
+    hamburger.classList.toggle("ri-close-large-line")
+})
+
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.toggle("left-[0]")
+        hamburger.classList.toggle('ri-close-large-line')
+        
+    })
+})
+
+
+// Show Scroll UP
+
+// Change Background Header
+
+// Swiper
+
+// Scroll sections active link
+
+// Scroll reveal animation
+
